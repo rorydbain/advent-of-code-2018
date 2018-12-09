83ee5371d9b2edaf9216db9f4903f89e80d33d81
Rory Bain <rorydbain@gmail.com>
2018-12-09 12:41:37 +0000

    working by avoiding recursion

--- day-5/main.py
+++ day-5/main.py
@@ -2,7 +2,7 @@
 with open('input.txt') as f:
 	content = f.readlines()[0]
 
-content = content.strip()[:980]
+content = content.strip()
 print("There are {cout} chars in total".format(cout=len(content)))
 
 def willReact(char_a, char_b):
@@ -10,20 +10,22 @@ def willReact(char_a, char_b):
 
 def strip_reacting_chars(index, string):
 	if index == len(string) - 1:
-		return string
+		return (string, None)
 	elif index < 0:
-		return strip_reacting_chars(index + 1, string)
+		return (string, index + 1)
 	
 	current_char = string[index]
 	next_char = string[index + 1]
 	
 	if willReact(current_char, next_char):
 		new_string = string[:index] + string[index + 2:]
-		return strip_reacting_chars(index - 1, new_string)
+		return (new_string, index - 1)
 	else:
-		return strip_reacting_chars(index + 1, string)
-		
-stripped = strip_reacting_chars(0, content)
+		return (string, index + 1)
+
+stripped, index = strip_reacting_chars(0, content)
+while index is not None:
+	stripped, index = strip_reacting_chars(index, stripped)
 
 print(stripped)
 print("number of characters:{count}".format(count=len(stripped)))
